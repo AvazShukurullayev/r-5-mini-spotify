aa0851e5061f44af8faa183aaf86f7fabe389d48
Avaz <elonarm21@gmail.com>
2023-11-02 21:03:16 +0500

    music player currentTime and duration is done

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -1,9 +1,13 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faAngleLeft, faAngleRight, faPause, faPlay} from "@fortawesome/free-solid-svg-icons";
 
 const Player = ({currentSong, isPlaying, setIsPlaying}) => {
+    // hooks
     const audioRef = useRef(null)
+    const [songInfo, setSongInfo] = useState({currentTime: null, duration: null})
+
+    // functions
     const playSongHandler = () => {
         if (isPlaying) {
             audioRef.current.pause()
@@ -12,14 +16,23 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
         }
         setIsPlaying(!isPlaying)
     }
+    const songUpdateHandler = (e) => {
+        const currentTime = e.target.currentTime
+        const duration = e.target.duration
+        setSongInfo((prevState) => ({...prevState, currentTime, duration}))
+    }
+    const readableTime = (time) => {
+        return Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    }
+
     return (
         <div className={"container"}>
             <div className="row">
                 <div className="col-6 mx-auto">
                     <div className="time-control d-flex align-items-center justify-content-between">
-                        <p className={"m-0 text-truncate"}>Start time</p>
-                        <input type="range" className={"w-50"} name="" id=""/>
-                        <p className={"m-0 text-truncate"}>End time</p>
+                        <p className={"m-0 me-1"}>{readableTime(songInfo.currentTime)}</p>
+                        <input type="range" className={"form-range"}/>
+                        <p className={"m-0 ms-1"}>{readableTime(songInfo.duration)}</p>
                     </div>
                     <div className="player-control py-5 d-flex align-items-center justify-content-between">
                         <FontAwesomeIcon className={"play"} size={"2x"} icon={faAngleLeft}/>
@@ -27,7 +40,7 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
                                          icon={isPlaying ? faPause : faPlay}/>
                         <FontAwesomeIcon className={"play"} size={"2x"} icon={faAngleRight}/>
                     </div>
-                    <audio src={currentSong.audio} ref={audioRef}></audio>
+                    <audio onTimeUpdate={songUpdateHandler} onLoadedMetadata={readableTime} src={currentSong.audio} ref={audioRef}></audio>
                 </div>
             </div>
         </div>
